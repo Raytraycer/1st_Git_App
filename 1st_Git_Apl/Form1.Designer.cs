142f458e66ba1595ec309af64fba5b10da513aa9
Pavel Kostin <Raytraycer@gmail.com>
2012-12-11 07:32:26 +0400

    stage 4

--- 1st_Git_Apl/Form1.Designer.cs
+++ 1st_Git_Apl/Form1.Designer.cs
@@ -29,26 +29,61 @@
         private void InitializeComponent()
         {
             this.lb_CurrentCursorPosition = new System.Windows.Forms.Label();
+            this.dg_OrderData = new System.Windows.Forms.DataGridView();
+            this.Data = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dg_OrderData)).BeginInit();
             this.SuspendLayout();
             // 
             // lb_CurrentCursorPosition
             // 
             this.lb_CurrentCursorPosition.AutoSize = true;
-            this.lb_CurrentCursorPosition.Location = new System.Drawing.Point(13, 8);
+            this.lb_CurrentCursorPosition.Location = new System.Drawing.Point(-1, 515);
             this.lb_CurrentCursorPosition.Name = "lb_CurrentCursorPosition";
             this.lb_CurrentCursorPosition.Size = new System.Drawing.Size(111, 13);
             this.lb_CurrentCursorPosition.TabIndex = 0;
             this.lb_CurrentCursorPosition.Text = "Waiting market coord.";
+            // 
+            // dg_OrderData
+            // 
+            this.dg_OrderData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dg_OrderData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg_OrderData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Data,
+            this.Value});
+            this.dg_OrderData.Location = new System.Drawing.Point(2, 12);
+            this.dg_OrderData.Name = "dg_OrderData";
+            this.dg_OrderData.ReadOnly = true;
+            this.dg_OrderData.RowHeadersVisible = false;
+            this.dg_OrderData.Size = new System.Drawing.Size(166, 150);
+            this.dg_OrderData.TabIndex = 1;
+            // 
+            // Data
+            // 
+            this.Data.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Data.HeaderText = "Data";
+            this.Data.Name = "Data";
+            this.Data.ReadOnly = true;
+            this.Data.Width = 50;
+            // 
+            // Value
+            // 
+            this.Value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(170, 537);
+            this.Controls.Add(this.dg_OrderData);
             this.Controls.Add(this.lb_CurrentCursorPosition);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg_OrderData)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +92,9 @@
         #endregion
 
         private System.Windows.Forms.Label lb_CurrentCursorPosition;
+        private System.Windows.Forms.DataGridView dg_OrderData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
     }
 }
 
--- 1st_Git_Apl/Form1.cs
+++ 1st_Git_Apl/Form1.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using Utilities;
 using System.Runtime.InteropServices;
 using WindowsInput;
+using System.IO;
 
 namespace _1st_Git_Apl
 {
@@ -29,10 +30,27 @@ namespace _1st_Git_Apl
         };
         #endregion
 
+        ListView SpisokFilov = new ListView();
+        List<string> SpisokStrok = new List<string>();
+        DirectoryInfo dir = new DirectoryInfo("Marketlogs");
+        string[,] ArrayMain = new string[500, 50];
+
+
+
 
         public Form1()
         {
             InitializeComponent();
+            dg_OrderData.Rows.Add();
+            dg_OrderData.Rows.Add();
+            dg_OrderData.Rows.Add();
+            dg_OrderData.Rows.Add();
+            dg_OrderData.Rows[0].Cells[0].Value = "SellPrice";
+            dg_OrderData.Rows[1].Cells[0].Value = "BuyPrice";
+            dg_OrderData.Rows[2].Cells[0].Value = "DirectDiff";
+            dg_OrderData.Rows[3].Cells[0].Value = "All taxes";
+            dg_OrderData.Rows[4].Cells[0].Value = "Real diff";
+            UpdateFont();
         }
 
         #region global hot keys
@@ -51,12 +69,12 @@ namespace _1st_Git_Apl
             //mouse_ClickToExportToMarket_Button();
             if (e.KeyCode.ToString() == "NumPad7")
             {
-                get_MarketButtonCoordinatos();
+                get_SellPriceToClipBoard();
             }
 
             if (e.KeyCode.ToString() == "NumPad8")
             {
-                mouse_ClickToExportToMarket_Button();
+                get_BuyPriceToClipBoard();
             }
             e.Handled = true;
         }
@@ -70,8 +88,8 @@ namespace _1st_Git_Apl
 
 
 
-        
-       
+
+        /*
         #region функции управления мышью
 
         float X_coef = 65535 / 1920; // устанавливаю глобальные коэфициенты отношений абсолютных координат к разрешению экрана. 
@@ -88,11 +106,11 @@ namespace _1st_Git_Apl
             int Abs_Y = Convert.ToInt32(Y_marketBtn * Y_coef);
             Cursor.Position = new Point(X_marketBtn, Y_marketBtn);
             mouse_event(MouseFlags.Absolute | MouseFlags.LeftDown, Abs_X, Abs_Y, 0, UIntPtr.Zero);
-            mouse_event(MouseFlags.Absolute | MouseFlags.LeftUp  , Abs_X, Abs_Y, 0, UIntPtr.Zero); 
+            mouse_event(MouseFlags.Absolute | MouseFlags.LeftUp, Abs_X, Abs_Y, 0, UIntPtr.Zero);
         }
         #endregion
 
-
+        
         void get_MarketButtonCoordinatos()
         {
             X_marketBtn = Control.MousePosition.X;
@@ -100,13 +118,209 @@ namespace _1st_Git_Apl
             lb_CurrentCursorPosition.Text = ("Market Btn.: X:" + X_marketBtn.ToString() + " - Y:" + Y_marketBtn.ToString());
 
         }
+        */
+
 
         
+        
 
-        public static void DoMouseClick()
+        public void get_TypeOfCalculation()
         {
+            // Обрабатываю список файлов в зависимости от кол-ва найденных файлов в папке
             
+            // завожу списки для импорта файла маркета
+            SpisokFilov.Clear();
+            SpisokStrok.Clear();
+            
+
+            // Собираю список файлов из директории.
+            foreach (FileInfo files in dir.GetFiles())
+            {
+                SpisokFilov.Items.Add(files.Name);
+            }
+
+
+
+            // если файл один - значит просто беру из него данные о ценах в текущей локации
+            if (SpisokFilov.Items.Count == 1)
+            {
+                get_Sell_Buy_PricesFromFile();
+            }
+            if (SpisokFilov.Items.Count == 0)
+            {
+                MessageBox.Show("Nothing to read.");
+            }
+
         }
+
+
+
+
+        double SellPrice = 999999999999;
+        double BuyPrice = 0;
+        
+       //dg_OrderData.Rows.Add();
+          //      
+
+
+
+
+
+
+        public void get_SellPriceToClipBoard()
+        {
+            get_TypeOfCalculation();
+            Clipboard.SetText(Convert.ToString(SellPrice));
+        }
+
+
+
+        public void get_BuyPriceToClipBoard()
+        {
+            get_TypeOfCalculation();
+            Clipboard.SetText(Convert.ToString(BuyPrice));
+        }
+
+
+
+
+
+        public void get_Sell_Buy_PricesFromFile()
+        {
+            // завожу списки для импорта файла маркета
+            
+            for (int i = 0; i < 500; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    ArrayMain[i, j] = "";
+                }
+            }
+           
+
+
+            string File_Name = SpisokFilov.Items[0].ToString();
+            string[] F_cut = File_Name.Split('{');
+
+            File_Name = F_cut[1].Trim('}');
+            if (File.Exists("Marketlogs/" + File_Name))
+            {
+                try //Считываю все строки.
+                {
+                    FileStream fs = new FileStream("Marketlogs/" + File_Name, FileMode.Open, FileAccess.Read);
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string str;
+                        while ((str = sr.ReadLine()) != null)
+                        {
+                            SpisokStrok.Add(str);
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Couldnt read from file.\n");
+                }
+
+                //string[,] ArrayMain = new string[SpisokStrok.Count,50]; // просто задаю размерность
+                for (int t = 0; t < SpisokStrok.Count; t++)
+                {
+                    ArrayMain[t, 0] = SpisokStrok[t];
+                    string[] BufferLine = SpisokStrok[t].Split(',');
+                    int r = 0;
+                    foreach (string item in BufferLine)
+                    {
+                        ArrayMain[t, r] = BufferLine[r];
+                        r++;
+                    }
+                }
+            }
+            #region поиск минимльной/максимальной цены.
+            SellPrice = 999999999999;
+            BuyPrice = 0;
+
+            for (int i = 0; i < 500; i++)
+            {
+                if (ArrayMain[i, 7] == "False")
+                {
+                    if (ArrayMain[i, 13] == "0")
+                    {
+                        if (Convert.ToDouble(ArrayMain[i, 0]) < SellPrice)
+                        {
+                            SellPrice = Convert.ToDouble(ArrayMain[i, 0]) - 0.01;
+                           // MessageBox.Show(Convert.ToString(SellPrice));
+                            //Clipboard.SetText(Convert.ToString(SellPrice));
+                        }
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < 500; i++)
+            {
+                if (ArrayMain[i, 7] == "True")
+                {
+                    if (ArrayMain[i, 13] == "0")
+                    {
+                        if (Convert.ToDouble(ArrayMain[i, 0]) > BuyPrice)
+                        {
+                            BuyPrice = Convert.ToDouble(ArrayMain[i, 0]) + 0.01;
+
+                        }
+                    }
+                }
+            }
+
+
+            const string dir = @"Marketlogs";
+            foreach (string file in Directory.GetFiles(dir))
+            //File.Delete(file);
+
+            dg_OrderData.Rows[0].Cells[1].Value = get_UserFrendlyViewOfPrice(SellPrice.ToString());
+            dg_OrderData.Rows[1].Cells[1].Value = get_UserFrendlyViewOfPrice(BuyPrice.ToString());
+            UpdateFont();
+            #endregion
+        }
+
+        public string get_UserFrendlyViewOfPrice(string Price)
+        {
+            double dprice = Convert.ToDouble(Price);
+            int billions = ((int)dprice / 1000000000);
+            int millions = ((int)(dprice-billions*1000000000) / 1000000);
+            int thousands= ((int)(dprice - billions*1000000000- millions*1000000) / 1000);
+            int pcs      = ((int)(dprice - billions*1000000000- millions*1000000 -thousands*1000));
+            
+            //преобразовываю в стринг с добавлением нулей перед разрядами для красоты
+            string sbillions = get_CorrectPresentationOfPrices(billions.ToString());
+            string smillions = get_CorrectPresentationOfPrices(millions.ToString());
+            string sthousands = get_CorrectPresentationOfPrices(thousands.ToString());
+            string spcs = get_CorrectPresentationOfPrices(pcs.ToString());
+            string sprice = sbillions + "B " + smillions + "M " + sthousands + "K " + spcs + "isk";
+            return sprice;
+        }
+
+        public string get_CorrectPresentationOfPrices (string sPrice)
+        {
+            while (sPrice.Length < 3)
+            {
+                sPrice = "0" + sPrice;
+            } 
+            return sPrice;
+        }
+
+
+        public void UpdateFont()
+        {
+            //Change cell font
+            foreach (DataGridViewColumn c in dg_OrderData.Columns)
+            {
+                c.DefaultCellStyle.Font = new Font("Arial", 8.5F, GraphicsUnit.Pixel);
+            }
+        }
+
+
+
+
 
     }
 }
